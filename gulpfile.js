47f09ec9fa3470a910a97a20e7ed9bcd421d2785
Steven Adler <steven_adler@gmx.de>
2020-04-30 13:44:15 +0200

    whitespace optimized

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,6 @@
 const gulp = require('gulp')
-const concat = require('gulp-concat')
 const sass = require('gulp-sass')
 const browserSync = require('browser-sync').create()
-const imagemin = require('gulp-imagemin')
-const terser = require('gulp-terser')
-const sourcemaps = require('gulp-sourcemaps')
-const postcss = require('gulp-postcss')
-const autoprefixer = require('autoprefixer')
-const cssnano = require('cssnano')
-const { src, series, dest } = require('gulp')
 
 // Compile SASS to CSS
 const style = () => {
@@ -30,43 +22,5 @@ const watch = () => {
     gulp.watch('./js/**/*.js').on('change', browserSync.reload)
 }
 
-// Copy HTML to dist folder
-const copyHTML = () => {
-    return src('./html/*.html')
-        .pipe(gulp.dest('dist'))
-}
-
-// Minify images
-const imageMinify = () => {
-    return src('./images/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./dist/images'))
-}
-
-// Minify js files
-const jsMinify = () => {
-    return src('./js/**/*.js')
-        .pipe(sourcemaps.init())
-        .pipe(concat('all.js'))
-        .pipe(terser())
-        .pipe(sourcemaps.write('.'))
-        .pipe(dest('./dist'))
-}
-
-// Minify css files
-const cssMinify = () => {
-    return src('./styles/css/**/*.css')
-        .pipe(sourcemaps.init())
-        .pipe(concat('style.css'))
-        .pipe(postcss([autoprefixer(), cssnano()]))
-        .pipe(sourcemaps.write('.'))
-        .pipe(dest('./dist'))
-}
-
 exports.style = style
 exports.watch = watch
-exports.copyHTML = copyHTML
-exports.imageMinify = imageMinify
-exports.jsMinify = jsMinify
-exports.cssMinify = cssMinify
-exports.build = series(style, copyHTML, imageMinify, jsMinify, cssMinify)
